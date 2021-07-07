746c5126d1887618dbf9bf4e7106e15e2f047fa1
Stu <gera-stu@ya.ru>
2021-07-07 10:09:22 +0300

    Initial Commit

---
+++ Pryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by Сэнди Белка on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
